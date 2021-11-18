7af1743662496b026fc07cb85055eec1d6fd0d33
jdt627 <90560254+jdt627@users.noreply.github.com>
2021-11-18 16:26:08 -0500

    Add files via upload

--- resolvers.ts
+++ resolvers.ts
@@ -1,29 +1,5 @@
-import { ModeloAvance } from './avance';
+import { resolversProyecto } from '../models/proyecto/resolvers';
+import { resolversUsuario } from '../models/usuario/resolvers';
+import { resolversAvance } from '../models/avance/resolvers';
 
-const resolversAvance = {
-  Query: {
-    Avances: async (parent, args) => {
-      const avances = await ModeloAvance.find().populate('proyecto').populate('creadoPor');
-      return avances;
-    },
-    filtrarAvance: async (parents, args) => {
-      const avanceFiltrado = await ModeloAvance.find({ proyecto: args.idProyecto })
-        .populate('proyecto')
-        .populate('creadoPor');
-      return avanceFiltrado;
-    },
-  },
-  Mutation: {
-    crearAvance: async (parents, args) => {
-      const avanceCreado = ModeloAvance.create({
-        fecha: args.fecha,
-        descripcion: args.descripcion,
-        proyecto: args.proyecto,
-        creadoPor: args.creadoPor,
-      });
-      return avanceCreado;
-    },
-  },
-};
-
-export { resolversAvance };
+export const resolvers = [resolversUsuario, resolversProyecto, resolversAvance];
